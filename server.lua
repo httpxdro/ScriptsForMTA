2892a886dc7913ef0a3538463dd3582a797fc874
Pedro <91342788+httpxdro@users.noreply.github.com>
2021-10-17 20:44:15 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-addCommandHandler('skin',
+addCommandHandler('(seu comando vai aqui)',
   function( player, cmd )
     outputChatBox('#(cor de hex vai aqui)(seu texto vai aqui)', player, 255, 105, 180 , true)
 end
